678166c0ffede31761a6d845117144fa966e1427
hweechieh <hweechieh14@gmail.com>
2019-11-30 13:14:43 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,17 +2,31 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
+//var cardId = cards.index;
 
-var cardOne = cards[0];
+//Code has been shortened and optimized by line 15 - 20.
+/*var cardOne = cards[0];
 cardsInPlay.push(cardOne);
-console.log("User flipped queen");
+console.log("User flipped " + cardOne);
 
 var cardTwo = cards[4];
 cardsInPlay.push(cardTwo);
-console.log("user flipped king");
+console.log("user flipped " + cardTwo);*/
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  	console.log("You found a match!");
+	} else {
+  	console.log("Sorry, try again.");
 }
+
+}
+function flipCard(cardId) {
+	checkForMatch();
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+}
+
+// Important note: not call the function within the function itself.
+flipCard(1);
+flipCard(2);
