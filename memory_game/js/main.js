8f8bf176c7f58db6901b7284a667233a16755df0
hweechieh <hweechieh14@gmail.com>
2019-12-01 00:16:37 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,28 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+},
+]
+
 var cardsInPlay = [];
 //var cardId = cards.index;
 
@@ -15,18 +37,22 @@ console.log("user flipped " + cardTwo);*/
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  	console.log("You found a match!");
+  	alert("You found a match!");
 	} else {
-  	console.log("Sorry, try again.");
+  	alert("Sorry, try again.");
 }
 
 }
 function flipCard(cardId) {
 	checkForMatch();
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[0].cardImage);
+	console.log(cards[0].suit);
 }
+
 
 // Important note: not call the function within the function itself.
 flipCard(1);
 flipCard(2);
+
